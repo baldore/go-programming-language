db446652c6eeec1377cb1f2b39eb95620491061d
baldore <acornssn@gmail.com>
2021-03-09 16:46:12 -0500

    ch1/fetch: Completes ex. 1.8 and 1.9

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -6,16 +6,25 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		hasPrefix := strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")
+
+		if !hasPrefix {
+			url = "https://" + url
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 		defer resp.Body.Close()
+
+		fmt.Printf("Status code: %v\n", resp.Status)
 
 		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
